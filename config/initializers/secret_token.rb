6577e5790339db563473065bd37eb3cf66d8e558
stanislav.knyazev <knyazev@ziex.by>
2012-09-26 10:23:10 +0300

    RSpec and Capybara added to project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StaticPages::Application.config.secret_token = '0cf97beb97a72c6f6d3ae11710e9101be5c65a1850318335b2dc90bd22f22b7396ff2ad8680e04c4addbd382f60600a4cf534f43aeb68f16285b3822e110dd16'
