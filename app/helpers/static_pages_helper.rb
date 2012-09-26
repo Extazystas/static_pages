6ed863889846f5dc6470d87ebd8cef2497644ddd
stanislav.knyazev <knyazev@ziex.by>
2012-09-26 16:23:15 +0300

    Improving Who we are

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,8 +1,12 @@
 module StaticPagesHelper
   def gravatar_url(var)
-    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(var)}?d=monsterid&s=150"
+    image_tag(("http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(var)}?d=monsterid&s=150"), :class => "img-polaroid")
+    
   end
   
+  def gravatar_name(var)
+    take_name = var.match(/\A((?:[a-zA-Z0-9.]+))/)
+  end
 
   def main_menu(cl)
     content_tag :ul, :class => cl do
