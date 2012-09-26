6b29b17f6b353a3b4348cb6acb0a8958c057b69c
stanislav.knyazev <knyazev@ziex.by>
2012-09-26 11:36:58 +0300

    Main menu links added

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,9 +8,15 @@ describe "StaticPages" do
       page.should have_selector('title', text: 'Knyazev Static: Homepage')
     end
 
-     it 'should have welcome content' do 
+    it 'should have welcome content' do 
       visit '/static_pages/home'
       page.should have_content('Welcome!')
+    end
+
+    it "should have link to about page" do
+      visit '/static_pages/home'
+      click_link 'About'
+      current_path.should == '/static_pages/about'
     end
   end
 
@@ -24,6 +30,13 @@ describe "StaticPages" do
       visit '/static_pages/about'
       page.should have_selector('title', text: 'Knyazev Static: About page')
     end
+
+    it "should have link to home page" do
+      visit '/static_pages/about'
+      click_link 'Home'
+      current_path.should == '/static_pages/home'
+    end
+
   end
 
 end
